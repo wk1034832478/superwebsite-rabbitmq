30a429b1e9e01a9a627998694603fa681226bb10
wangkai <1034832478@qq.com>
2019-03-30 19:24:09 +0800

    index.html

--- src/main/java/home/hyywk/top/superwebsiterabbitmq/services/Processor.java
+++ src/main/java/home/hyywk/top/superwebsiterabbitmq/services/Processor.java
@@ -3,7 +3,6 @@ package home.hyywk.top.superwebsiterabbitmq.services;
 import home.hyywk.top.superwebsiterabbitmq.entity.Message;
 
 import javax.websocket.Session;
-import java.util.List;
 import java.util.Set;
 
 /**
